4b4cce62cec851f97ba6f80ec0a903fb02e491b9
Lewaa Bahmad <lewaabahmad@gmail.com>
2019-04-05 19:01:12 -0400

    add search

--- app/models/zapi_app/model.rb
+++ app/models/zapi_app/model.rb
@@ -1,11 +1,19 @@
 class ZapiApp::Model
 
-  def self.fetch(args)
+  def self.find(args)
     instance_id = args[:instance_id]
     url = URI.parse("https://www.zapi.app/api/v1/instances/#{instance_id}?key=#{ENV['ZAPI_PRIVATE_KEY']}")
     res = Net::HTTP.get(url)
     i = JSON.parse(res)["data"]
     OpenStruct.new(i["attributes"].except("data").merge(i["attributes"]["data"]))
+  end
+
+  def find_by(args)
+    generated_params = args.map { |k, v| "#{k}=#{v}" }.join('&')
+    url = URI.parse("https://www.zapi.app/api/v1/search?key=#{ENV['ZAPI_PRIVATE_KEY']}&#{generated_params}")
+    res = Net::HTTP.get(url)
+    instances = JSON.parse(res)["data"]
+    instances.map { |i| OpenStruct.new(i["attributes"].except("data").merge(i["attributes"]["data"])) }
   end
 
   def self.model_struct
