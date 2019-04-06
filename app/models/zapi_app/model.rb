97e9d48e37d111b161ca4920ad87546bd9822a81
Lewaa Bahmad <lewaabahmad@gmail.com>
2019-04-05 20:40:40 -0400

    add all and refactor

--- app/models/zapi_app/model.rb
+++ app/models/zapi_app/model.rb
@@ -1,7 +1,6 @@
 class ZapiApp::Model
 
   def initialize(model_id, args={})
-    binding.pry
     @model_id = model_id
   end
 
@@ -11,16 +10,16 @@ class ZapiApp::Model
 
   def find_by(args)
     generated_params = args.map { |k, v| "#{k}=#{v}" }.join('&')
-    url = URI.parse("https://www.zapi.app/api/v1/search?key=#{ENV['ZAPI_PRIVATE_KEY']}&#{generated_params}")
+    url = URI.parse("https://www.zapi.app/api/v1/models/#{@model_id}/search?key=#{ENV['ZAPI_PRIVATE_KEY']}&#{generated_params}")
     res = Net::HTTP.get(url)
     instances = JSON.parse(res)["data"]
     instances.map { |i| OpenStruct.new(i["attributes"].except("data").merge(i["attributes"]["data"])) }
   end
 
   def all
-    url = URI.parse("https://www.zapi.app/api/v1/model/#{@model_id}/instances?key=#{ENV['ZAPI_PRIVATE_KEY']}")
+    url = URI.parse("https://www.zapi.app/api/v1/models/#{@model_id}/instances?key=#{ENV['ZAPI_PRIVATE_KEY']}")
     res = Net::HTTP.get(url)
-    JSON.parse(res.body)["data"].map do |i|
+    JSON.parse(res)["data"].map do |i|
       OpenStruct.new(i["attributes"].except("data").merge(i["attributes"]["data"]))
     end
   end
